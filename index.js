6fd1626d0e16380a4b0f211b0240ebfba3bf462a
unknown <whiy07@gmail.com>
2023-11-15 04:19:48 +0000

    add a post request to create a user

--- index.js
+++ index.js
@@ -40,8 +40,10 @@ app.get('/', (req, res) => {
 app.post('/api/users', async (req, res) => {
   try {
     const { username } = req.body
+    const user = await User.create({ username })
+    res.status(201).json({ user })
   } catch (error) {
-    
+    res.status(500).json({ message: "Internal Server Error" });
   }
 })
 
