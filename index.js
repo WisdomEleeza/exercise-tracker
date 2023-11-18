5947d0aacef1890732fd475097dc8b4d8e6f0fcd
unknown <whiy07@gmail.com>
2023-11-18 13:38:29 +0000

    update route to post exercise(2)

--- index.js
+++ index.js
@@ -169,7 +169,6 @@ app.get('/api/users/:_id/logs', async (req, res) => {
 
 
 
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
